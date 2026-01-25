8825b60319e560e966c33611eb8688593d6fcd0c
TitsMcGee-del <misitoth@gmail.com>
2026-01-25 21:18:57 +0100

    Update bets.js

--- functions/bets.js
+++ functions/bets.js
@@ -1,1 +1,23 @@
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+
+exports.placeBet = functions.https.onRequest(async (req, res) => {
+  const { user, matchId, amount } = req.body;
+
+  if (!user || !matchId || !amount) {
+    return res.status(400).send("Missing data");
+  }
+
+  const betRef = admin.database().ref("bets").push();
+
+  await betRef.set({
+    user,
+    matchId,
+    amount,
+    status: "active",
+    placedAt: Date.now()
+  });
+
+  res.json({ success: true, betId: betRef.key });
+});
 

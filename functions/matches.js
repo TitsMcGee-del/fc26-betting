92cd8e9ee1563c7e066dabeabb01728424d82bf7
TitsMcGee-del <misitoth@gmail.com>
2026-01-25 21:40:27 +0100

    Update matches.js

--- functions/matches.js
+++ functions/matches.js
@@ -1,1 +1,22 @@
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+
+exports.createMatch = functions.https.onRequest(async (req, res) => {
+  const { home, away, odds } = req.body;
+
+  if (!home || !away || !odds) {
+    return res.status(400).send("Missing data");
+  }
+
+  const matchRef = admin.database().ref("matches").push();
+
+  await matchRef.set({
+    home,
+    away,
+    odds,
+    createdAt: Date.now()
+  });
+
+  res.json({ success: true, matchId: matchRef.key });
+});
 

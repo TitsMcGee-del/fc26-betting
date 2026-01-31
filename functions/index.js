eb106cf565aff0d487942c90b6426522d042e7a3
TitsMcGee-del <misitoth@gmail.com>
2026-01-31 14:28:55 +0100

    Delete functions directory

--- functions/index.js
+++
@@ -1,9 +0,0 @@
-const functions = require("firebase-functions");
-const admin = require("firebase-admin");
-
-admin.initializeApp();
-
-exports.placeBet = require("./bets").placeBet;
-exports.cashOut = require("./cashout").cashOut;
-exports.createMatch = require("./matches").createMatch;
-
